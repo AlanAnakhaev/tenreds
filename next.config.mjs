737e34b96727259617259c9fd5eb8acc8ffc4d57
Alan Anakhaev <alananakhaev@gmail.com>
2024-08-12 16:20:03 +0600

    ci changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,12 @@
 import withPWA from "@ducanh2912/next-pwa";
-import { withExport } from "next-export"; // This is for handling static export
+import { withExport } from "next-export";
 
 const pwaConfig = withPWA({
   dest: "public",
-  // Any additional PWA config options you need
 });
 
 const nextConfig = {
-  // Ensure export mode is enabled
   output: "export",
-  // You might need to add other Next.js configuration options here
 };
 
 export default withExport(withPWA(nextConfig, pwaConfig));
